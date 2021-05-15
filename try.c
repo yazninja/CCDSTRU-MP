3fe184bc47ccbd6e50589cbdf9714b4cba14b15f
AyishaDytoc <>
2021-05-15 11:14:36 +0000

    added variables

--- try.c
+++ try.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 
 struct position{
-	int a, b;
+	int a, b; //values 1-4
 };
+
+typedef struct position Player;
 
 int main()
 {
 	int turn, over; // 1(true) or 0(false)
-
-
+	
+	Player Ord,Cha,Free;
   
   
   return 0;
